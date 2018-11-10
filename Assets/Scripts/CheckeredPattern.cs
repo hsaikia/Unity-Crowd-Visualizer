5160756e2a08992be459ab86da64ad5e2f973ce6
Himangshu <saikia@kth.se>
2018-11-10 17:41:32 +0100

    init

---
+++ Assets/Scripts/CheckeredPattern.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckeredPattern : MonoBehaviour {
+
+	public Texture2D texture;
+	public Color kola;
+	public Color boga;
+	public int sizeX;
+	public int sizeY;
+	// Use this for initialization
+	void Start () {
+		makeTexture();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void makeTexture(){
+		texture = new Texture2D(sizeX, sizeY);
+		for(int i = 0; i < sizeX; i++){
+			for(int j = 0; j < sizeY; j++){
+				if (((i + j)%2)==1)
+            	{
+                	texture.SetPixel(i, j, kola);
+            	}
+            	else
+            	{
+                	texture.SetPixel(i, j, boga);
+            	}
+			}
+		}
+		texture.Apply();
+		GetComponent<Renderer>().material.mainTexture = texture;
+		texture.wrapMode = TextureWrapMode.Clamp;
+		texture.filterMode = FilterMode.Point;
+	}
+
+}
---
+++ Assets/Scripts/SWCrowd.cs
@@ -0,0 +1,240 @@
+using System.Collections;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Agent {
+    public float r;
+    public float g;
+    public float b;
+    public float radius;
+    public List<Vector3> pos;
+    public List<Vector3> tar;
+
+    Vector3 forw_;
+
+    public Agent(){
+        pos = new List<Vector3>();
+        tar = new List<Vector3>();
+    }
+
+    public Vector3 getForward(int t){
+        Vector3 newforw;
+        if(t == 0){
+           newforw = tar[0] - pos[0];
+        } else {
+            newforw = pos[t] - pos[t - 1];
+        }
+        if(newforw.magnitude < 0.00001){
+            return forw_;
+        }
+        forw_ = newforw;
+        return forw_;
+    }
+}
+
+public class SWCrowd : MonoBehaviour
+{
+    public bool showInfo;
+    public GameObject walkerPrefab_;
+    public GameObject targetPrefab_;
+    public TextAsset csvFile_;
+    private int numAgents_;
+    private List<Agent> agents_;
+    private List<GameObject> walkerAgents_;
+    private List<GameObject> targetAgents_;
+    public Text info_;
+    private int columns_;
+    private int timestep;
+    private int maxTimeStep;
+    public bool animate;
+
+    [Range (0, 100)]
+    public int trailLength;
+
+    void Start()
+    {
+        agents_ = new List<Agent>();
+        walkerAgents_ = new List<GameObject>();
+        targetAgents_ = new List<GameObject>();
+        readCSV();
+
+        if(animate){
+            InvokeRepeating("moveAgents", 0.0f, 0.1f);
+        }
+        //InvokeRepeating("moveAgents", 0.0f, 0.1f);
+    }
+
+    void Update()
+    {
+        info_.text = "Timestep " + timestep;
+    }
+
+    void OnDrawGizmos(){
+
+    }
+
+    void readCSV(){
+        char lineSeparator = '\n'; // It defines line seperate character
+        char fieldSeparator = ','; // It defines field seperate chracter
+
+        List<List<float> > dataitems = new List<List<float> >(); 
+
+        string[] records = csvFile_.text.Split(lineSeparator);
+
+        bool ignoreFirst = true;
+
+        foreach (string record in records)
+        {
+            string[] fields = record.Split(fieldSeparator);
+            //Debug.Log(record);
+            if(ignoreFirst){
+                columns_ = fields.GetLength(0);
+                ignoreFirst = false;
+                continue;
+            }
+
+            //Debug.Log("Columns extracted " + fields.GetLength(0));
+
+            if(fields.GetLength(0) != columns_){
+                continue;
+            }
+
+            List<float> row = new List<float>();
+
+            int c = 0;
+            foreach(string field in fields)
+            {
+                if(c >= columns_){
+                    break;
+                }
+                //Debug.Log("Field " + field);
+                row.Add(float.Parse(field));       
+                c++;
+            }
+
+            dataitems.Add(row);
+        }
+        
+        for(int i = 0; i < dataitems.Count; i++){
+            // TIME ,ID ,POS_X ,POS_Y, TAR_X, TAR_Y, AGENT_RADIUS, COLOR_R, COLOR_G, COLOR_B 
+            int id = Convert.ToInt32(dataitems[i][1]);
+            int time = Convert.ToInt32(dataitems[i][0]);
+
+            maxTimeStep = Mathf.Max(maxTimeStep, time);
+
+            while(id + 1 > agents_.Count){
+                agents_.Add(new Agent());
+            }
+            
+            agents_[id].pos.Add(new Vector3(dataitems[i][2], 0, dataitems[i][3]));
+            agents_[id].tar.Add(new Vector3(dataitems[i][4], 0, dataitems[i][5]));
+            agents_[id].radius = dataitems[i][6];
+            agents_[id].r = dataitems[i][7];
+            agents_[id].g = dataitems[i][8];
+            agents_[id].b = dataitems[i][9];
+        }
+
+        //Debug.Log("Counts [" + dataitems.Count + "," + dataitems[0].Count + "]");
+
+        // List<List<double> > data = new List<List<double> >(); 
+
+        // for(int i = 0; i < dataitems[0].Count; i++){
+        //     List<double> col = new List<double>();
+        //     for(int j = 0; j < dataitems.Count; j++){
+        //         col.Add(dataitems[j][i]);
+        //     }
+        //     data.Add(col);
+        // }
+
+        // // Find number of agents
+
+        // int minId = 200000;
+        // int maxId = 0;
+
+        // // looping through agent_id
+        // for(int i = 0; i < data[1].Count; i++){
+        //     minId = Mathf.Min(minId, Convert.ToInt32(data[1][i]));
+        //     maxId = Mathf.Max(minId, Convert.ToInt32(data[1][i]));
+        // }
+
+        // Debug.Log("Min ID " + minId + " Max ID " + maxId);
+
+        numAgents_ = agents_.Count;
+
+        // set agents initial position
+
+        SetGameObjects();
+    }
+
+    void UpdateInfo(){
+        info_.text = "B.o.B Do something!";
+    }
+
+    void ClearScene(){
+
+        foreach(var wa in walkerAgents_){
+            Destroy(wa);
+        }
+
+        walkerAgents_.Clear();
+
+        foreach(var tar in targetAgents_){
+            Destroy(tar);
+        }
+
+        targetAgents_.Clear();
+    }
+
+    void SetGameObjects()
+    {
+        ClearScene();
+        for(int i = 0; i < numAgents_; i++)
+        {
+            GameObject walkerClone = Instantiate(walkerPrefab_, agents_[i].pos[0] , Quaternion.identity);
+            walkerClone.GetComponent<MeshRenderer>().material.color = new Color(agents_[i].r, agents_[i].g, agents_[i].b);
+            walkerClone.transform.localScale = new Vector3(2 * agents_[i].radius, 1, 2 * agents_[i].radius);
+            walkerClone.transform.forward = agents_[i].getForward(0);
+            walkerClone.SetActive(true);
+            //line.startColor = new Color(agents_[i].r, agents_[i].g, agents_[i].b);
+            //line.endColor = new Color(agents_[i].r, agents_[i].g, agents_[i].b);
+            walkerClone.AddComponent(typeof(LineRenderer));
+            walkerClone.GetComponent<LineRenderer>().material = new Material(Shader.Find("Sprites/Default"));
+            walkerClone.GetComponent<LineRenderer>().startColor = new Color(agents_[i].r, agents_[i].g, agents_[i].b);
+            walkerClone.GetComponent<LineRenderer>().widthMultiplier = 0.2f;
+
+            GameObject targetClone = Instantiate(targetPrefab_, agents_[i].tar[0], Quaternion.identity);
+            targetClone.GetComponent<MeshRenderer>().material.color = new Color(agents_[i].r, agents_[i].g, agents_[i].b);
+            targetClone.transform.localScale = new Vector3(2 * agents_[i].radius, 1, 2 * agents_[i].radius);
+            targetClone.SetActive(true);
+
+            walkerAgents_.Add(walkerClone);
+            targetAgents_.Add(targetClone);
+        }
+    }
+
+    void moveAgents(){
+        timestep = (timestep + 1) % maxTimeStep;
+        for(int i = 0; i < numAgents_; i++)
+        {
+            walkerAgents_[i].transform.SetPositionAndRotation(agents_[i].pos[timestep], Quaternion.identity);
+            walkerAgents_[i].transform.forward = agents_[i].getForward(timestep);
+            targetAgents_[i].transform.SetPositionAndRotation(agents_[i].tar[timestep], Quaternion.identity);
+
+            List<Vector3> poss = new List<Vector3>();
+
+            for(int t = timestep; t >= 0; t--){
+                if(poss.Count >= trailLength){
+                    break;
+                }
+                poss.Add(agents_[i].pos[t]);
+            }
+
+            walkerAgents_[i].GetComponent<LineRenderer>().positionCount = poss.Count;
+            walkerAgents_[i].GetComponent<LineRenderer>().SetPositions(poss.ToArray());
+        }
+    }
+
+}
